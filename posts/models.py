823fdeb706aa99eb0a39b74c296af1ae502109d7
mostafa azam <mostafaazam@192.168.1.10>
2024-01-28 22:13:08 +0200

    design post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+   post:
+    - title
+    - content
+    - publish_data
+    - author
+    - image
+    - tags
+    - category
+    - comments
+'''
+class Post(models.Model):
+    title =  models.CharField(max_length=100)
+    content = models.TextField()
+    draft = models.BooleanField(default= True)
