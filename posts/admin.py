d4c432b1bd898431b7d124bd3fb441af52f77e1e
mostafa azam <mostafaazam@192.168.1.10>
2024-01-29 22:45:30 +0200

    add publish_date

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class productAdmin(admin.ModelAdmin):
+    list_display = ['title','draft']
+    list_filter = ['draft']
+    search_fields = ['title']
+    
+
+
+
+
+
+admin.site.register(Post,productAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -17,3 +18,12 @@ class Post(models.Model):
     title =  models.CharField(max_length=100)
     content = models.TextField()
     draft = models.BooleanField(default= True)
+    publish_date = models.DateTimeField(default=timezone.now)
+    
+    # create time auto
+    publish_date2 = models.DateTimeField(auto_now=True) 
+    
+    
+    def __str__(self):
+        return self.title
+        
